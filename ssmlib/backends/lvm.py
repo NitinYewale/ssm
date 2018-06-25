NitinYewale/ssm

commit 7449db6b52cf108a1844bcaf94a5efcf5ea8a60a

ssm: add a workaround for lvm and exported volumes
When lvm lvs command is run on exported volumes, lvm ends with return
code 5 instead of 0 on a clean run. Because LVM people said that
this is intentional behaviour, it needs to be worked around in SSM.

This patch adds a condition that detects the mentioned issue and sets a
can_fail flag, so the error code is ignored.

Signed-off-by: Jan Tulak <jtulak@redhat.com>

ssmlib/misc.py
@@ -450,6 +450,25 @@ def humanize_size(arg):
         unit = "???"
     return ("{0:.2f} {1}").format(size, unit)
 
+def _can_fail_hacks(cmd, returncode, error):
+    """ A workaround for tools reporting weird exit codes. Return true
+        when a non-zero return code should be ignored.
+    """
+
+    if returncode == 5 \
+        and cmd[0:2] == ['lvm', 'lvs'] \
+        and str(error).endswith('is exported\n'):
+        # FIXME A workaround for LVM behaviour:
+        # lvm lvs' exit code is 5 on exported volumes, even if everything
+        # is ok. So, if the code is 5, command was 'lvm lvs ...'
+        # and error message says that a volume was exported, set up can_fail
+        # flag.
+        # We can't use per-object status, because that would break backward
+        # compatibility. And we can't deal with it on a higher level, because
+        # we don't have all the information then.
+        return True
+
+    return False
 
 def run(cmd, show_cmd=False, stdout=False, stderr=True, can_fail=False,
         stdin_data=None, return_stdout=True):
@@ -493,6 +512,9 @@ def run(cmd, show_cmd=False, stdout=False, stderr=True, can_fail=False,
         stdin_thread.join()
 
     err_msg = "ERROR running command: \"{0}\"".format(" ".join(cmd))
+    if _can_fail_hacks(cmd, proc.returncode, error):
+        can_fail = True
+
     if proc.returncode != 0 and show_cmd:
         if output is not None:
             print(output)

commit b52555f33e7cacfa8d6b7b6a0ef39407c946c334

lvm: Move lvm specific error handling to lvm backend
Commit 7449db6b52cf108a1844bcaf94a5efcf5ea8a60a fixed the problem where
lvs would exit with error code 5 if it encountered exported volume
group. However this is very lvm specific hack that does belong to lvm
backend rather than to the generic code. Move it

Signed-off-by: Lukas Czerner <lczerner@redhat.com>
Fixes: 7449db6 ("ssm: add a workaround for lvm and exported volumes")

ssmlib/backends/lvm.py
@@ -110,7 +110,21 @@ class LvmInfo(template.Backend):
     def _parse_data(self, command):
         if not self.binary:
             return
-        self.output = misc.run(command, stderr=False)[1]
+        ret, self.output, err = misc.run(command, stderr=False, can_fail=True)
+        # A workaround for LVM behaviour:
+        # lvm lvs' exit code is 5 on exported volumes, even if everything
+        # is ok. So, if the code is 5, command was 'lvm lvs ...'
+        # and error message says that a volume was exported, ignore the
+        # error
+        if ret != 0:
+            err_msg = "ERROR exit code {0} for running command: \"{1}\"".format(
+                      ret, " ".join(command))
+            if ret != 5 or command[0:2] != ['lvm', 'lvs'] or \
+               not str(err).endswith('is exported\n'):
+                if err is not None:
+                    print(err)
+                raise problem.CommandFailed(err_msg, exitcode=ret)
+
         for line in self.output.split("\n"):
             if not line:
                 break

ssmlib/misc.py
@@ -462,25 +462,6 @@ def humanize_size(arg):
         unit = "???"
     return ("{0:.2f} {1}").format(size, unit)
 
-def _can_fail_hacks(cmd, returncode, error):
-    """ A workaround for tools reporting weird exit codes. Return true
-        when a non-zero return code should be ignored.
-    """
-
-    if returncode == 5 \
-        and cmd[0:2] == ['lvm', 'lvs'] \
-        and str(error).endswith('is exported\n'):
-        # FIXME A workaround for LVM behaviour:
-        # lvm lvs' exit code is 5 on exported volumes, even if everything
-        # is ok. So, if the code is 5, command was 'lvm lvs ...'
-        # and error message says that a volume was exported, set up can_fail
-        # flag.
-        # We can't use per-object status, because that would break backward
-        # compatibility. And we can't deal with it on a higher level, because
-        # we don't have all the information then.
-        return True
-
-    return False
 
 def run(cmd, show_cmd=False, stdout=False, stderr=True, can_fail=False,
         stdin_data=None, return_stdout=True):
@@ -510,10 +491,7 @@ def run(cmd, show_cmd=False, stdout=False, stderr=True, can_fail=False,
     output, error = proc.communicate(input=stdin_data)
 
     err_msg = "ERROR exit code {0} for running command: \"{1}\"".format(
-        proc.returncode,
-        " ".join(cmd))
-    if _can_fail_hacks(cmd, proc.returncode, error):
-        can_fail = True
+              proc.returncode, " ".join(cmd))
 
     if proc.returncode != 0 and show_cmd:
         if output is not None: