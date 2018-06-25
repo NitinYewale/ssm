NitinYewale/ssm

commit 9037c6d608ceb222865b999798eaf86d558f06af

btrfs: unhide devices in tests
When using filter and working with btrfs, like in bash tests, ssm
incorrectly hid some btrfs devices because of implicit hide logic.
Fix it by explicitly setting hide = False for btrfs devices - if a
backend recognizes some device, it is it's responsibility to show
it.

Signed-off-by: Jan Tulak <jtulak@redhat.com>

ssmlib/backends/btrfs.py
@@ -402,6 +402,9 @@ class BtrfsDev(Btrfs, template.BackendDevice):
         else:
             self.data = self._dev
 
+        for name, _ in self.data.items():
+            self.data[name]['hide'] = False
+
     def remove(self, devices):
         raise Exception("Not sure what you want to" +
                         "achieve by removing {0}".format(devices))

commit 86e30696fffdedc5226d0ba36c62bd50bdc10112

btrfs: Avoid unnecessary loops when initializing data
Previous commit 9037c6d608ce fixed the problem that the
btrfs devices can be hidden in ssm list if the device itself was the
type we hide by default.
However it's better to do it when we discover the device, rather then
loop over the devices again once we're done.

Signed-off-by: Lukas Czerner <lczerner@redhat.com>
Fixes: 9037c6d ("btrfs: unhide devices in tests")

ssmlib/backends/btrfs.py
@@ -153,6 +153,7 @@ class Btrfs(template.Backend):
                     int(partitions[dev['dev_name']][2])
                 pool_size += dev_size
                 dev['dev_free'] = dev_size - dev_used
+                dev['hide'] = False
                 self._dev[dev['dev_name']] = dev
                 dev = {}
 
@@ -401,9 +402,6 @@ class BtrfsDev(Btrfs, template.BackendDevice):
             self.data.update(self._dev)
         else:
             self.data = self._dev
-
-        for name, _ in self.data.items():
-            self.data[name]['hide'] = False
 
     def remove(self, devices):
         raise Exception("Not sure what you want to" +